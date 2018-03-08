87f5954ca6c09eb636a3c3dd94cb7f141926d02a
Roberto Oliveira <robertooliveira.dev@gmail.com>
2018-03-08 18:59:00 -0300

    Set vendor chunks

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -3,11 +3,17 @@ const CleanPlugin = require('clean-webpack-plugin')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 const baseConfig = require('./webpack.config.base')
+const packageJson = require('../package.json')
 
 const config = {
-
   ...baseConfig,
   mode: 'production',
+
+  entry: {
+    app: './src/index.js',
+    vendor: Object.keys(packageJson.dependencies)
+  },
+
   plugins: [
 
     ...baseConfig.plugins,
@@ -17,6 +23,16 @@ const config = {
     })
   ],
   optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: 'initial',
+          name: 'vendor',
+          test: 'vendor',
+          enforce: true
+        }
+      }
+    },
     runtimeChunk: true
   },
   output: {
