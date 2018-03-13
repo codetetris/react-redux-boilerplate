90cb46f46724fff9202307b1c3acd1553dc2e620
Renan Oliveira <ren4n.oliveir4@gmail.com>
2018-03-12 22:58:17 -0300

    fixed fontface

--- src/index.style.js
+++ src/index.style.js
@@ -1,14 +1,18 @@
 import emotionNormalize from 'emotion-normalize'
-import { injectGlobal, fontFace } from 'emotion'
-
-fontFace`
-  font-family: 'Oxygen';
-  font-style: normal;
-  font-weight: 400;
-  src: local('Oxygen Regular'), local('Oxygen-Regular'), url(https://fonts.gstatic.com/s/oxygen/v6/qBSyz106i5ud7wkBU-FrPevvDin1pK8aKteLpeZ5c0A.woff2) format('woff2');
-  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215;
-`
+import { injectGlobal } from 'emotion'
 
 injectGlobal`
   ${emotionNormalize}
+  @font-face {
+    font-family: 'Patrick Hand SC';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Patrick Hand SC'),
+      local('PatrickHandSC-Regular'),
+      url(https://fonts.gstatic.com/s/patrickhandsc/v4/OYFWCgfCR-7uHIovjUZXsZ71Uis0Qeb9Gqo8IZV7ckE.woff2)
+        format('woff2');
+    unicode-range: U+0100-024f, U+1-1eff,
+      U+20a0-20ab, U+20ad-20cf, U+2c60-2c7f,
+      U+A720-A7FF;
+  }
 `
