dff159bb403fb24ac580fefc271bae5cf500a55c
Renan Oliveira <ren4n.oliveir4@gmail.com>
2018-03-12 21:19:20 -0300

    fixed test

--- src/pages/__tests__/LoginPage.test.js
+++ src/pages/__tests__/LoginPage.test.js
@@ -3,11 +3,13 @@ import { shallow } from 'enzyme'
 
 import LoginPage from '../LoginPage'
 
-import configureStore from '../../store/configureStore'
+import configureStore from 'redux-mock-store'
+
+const mockStore = configureStore([])
 
 describe('(Component) LoginPage', () => {
   it('renders without crash', () => {
-    const store = configureStore({}).store
+    const store = mockStore({})
     const wrapper = shallow(<LoginPage store={store} />)
 
     expect(wrapper).toHaveLength(1)
